53ff6d8e1578e6122372bf748300833ee6b5ed03
revolman <1revolman1@gmail.com>
2019-11-05 14:49:45 +0200

    add

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -110,51 +110,6 @@ document.querySelector(".menu_btn").addEventListener("click", function(e) {
     ).offsetHeight + 1}px`;
 });
 
-function randomInteger(min, max) {
-  let rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
-}
-let element_visible = [
-  ` <div class="image"><img src="img/Jess 73 v1a@2x.png" alt=""></div>
-  <div class="content"> <h3>
-               Lorem ipdasdsum dolor sit amet consectetur adipisicing elit. Esse in dolores ducimus
-              </h3>
-              <span>Ricardo Milos</span>
-              <p>Dancer</p></div>`,
-  ` <div class="image"><img src="img/Jess 73 v1a@2x.png" alt="" /></div>
-           <div class="content"><h3>
-                “There isn’t anything else out there that’s like
-                thisislanguage.com”
-              </h3>
-              <span>Jess Lund</span>
-              <p>Head of MFL Michaela Community School</p></div>
-            `,
-  `<div class="image"><img src="img/Jess 73 v1a@2x.png"" alt="" /></div> <div class="content"><h3>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse in dolores ducimus
-              </h3>
-              <span>Jessus Christ</span>
-              <p>Head of Earth</p></div>`
-];
-let index = Math.round(element_visible.length / 2);
-document.querySelector(".block-with-content").innerHTML =
-  element_visible[index - 1];
-
-document.querySelector(".button-left").addEventListener("click", function(e) {
-  if (index - 1 != 0) {
-    index--;
-    document.querySelector(".block-with-content").innerHTML =
-      element_visible[index - 1];
-  }
-});
-document.querySelector(".button-right").addEventListener("click", function(e) {
-  if (index + 1 > element_visible.length) {
-  } else {
-    index++;
-    document.querySelector(".block-with-content").innerHTML =
-      element_visible[index - 1];
-  }
-});
-
 document.querySelectorAll(".slider .block").forEach(e => {
   e.style.background = `rgb(${randomInteger(0, 255)}, ${randomInteger(
     0,
@@ -167,7 +122,8 @@ $(document).on("ready", function() {
     arrows: false,
     dots: false,
     infinite: true,
-    centerMode: true,
+    variableWidth: true,
+    // centerMode: true,
     autoplay: true,
     autoplaySpeed: 2000,
     slidesToScroll: 3,
